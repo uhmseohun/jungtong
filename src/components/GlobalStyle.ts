cdaee163b5421708527b8de10d5a925e76651ffe
uhmseohun <uhmtotow@gmail.com>
2020-12-09 12:40:59 +0900

    feat: add global style

---
+++ src/components/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&display=swap');
+
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
